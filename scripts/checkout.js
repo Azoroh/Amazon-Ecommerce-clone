7086c960b5f003e28ba81c20dcf24c7864c447cd
Azoroh <cjaylambs@gmail.com>
2025-06-22 11:48:35 +0100

    delete products from html when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -17,7 +17,9 @@ cart.forEach((cartItem) => {
   console.log(matchingProduct);
 
   cartSummaryHTML += `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${
+      matchingProduct.id
+    }">
       <div class="delivery-date">
         Delivery date: Wednesday, June 15
       </div>
@@ -104,6 +106,10 @@ document.querySelectorAll(".js-delete-link").forEach((link) => {
     const { productId } = link.dataset;
 
     removeFromCart(productId);
-    console.log(cart);
+
+    const container = document.querySelector(
+      `.js-cart-item-container-${productId}`
+    );
+    container.remove();
   });
 });
