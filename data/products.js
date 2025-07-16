d1f5f93ff0ead12933c8fb84f518d93117ee0cd5
Azoroh <cjaylambs@gmail.com>
2025-07-16 15:51:01 +0100

    Use Inheritance and Polymorphism to add size chart

--- data/products.js
+++ data/products.js
@@ -34,6 +34,23 @@ class Product {
   getPrice() {
     return `$${formatCurrency(this.priceCents)}`;
   }
+  extraInfoHTML() {
+    return "";
+  }
+}
+
+class Clothing extends Product {
+  sizeChartLink;
+
+  constructor(productDetails) {
+    super(productDetails);
+    this.sizeChartLink = productDetails.sizeChartLink;
+  }
+
+  extraInfoHTML() {
+    // super.extraInfoHTML()
+    return `<a href= "${this.sizeChartLink}" target="_blank">Size chart</a>`;
+  }
 }
 
 //
@@ -545,5 +562,9 @@ export const products = [
     keywords: ["bottle", "shaker", "sports", "flask"],
   },
 ].map((productDetails) => {
+  if (productDetails.type === "clothing") {
+    return new Clothing(productDetails);
+  }
+
   return new Product(productDetails);
 });
