ca5e959eef53882e7e5470c58e4ecb9092e65d4a
Azoroh <cjaylambs@gmail.com>
2025-07-21 22:59:09 +0100

    rename amazon.html to index.html

--- scripts/checkoutHeader.js
+++ scripts/checkoutHeader.js
@@ -4,14 +4,14 @@ export function renderCheckoutHeader() {
   let checkoutHeaderHTML = `
     <div class="header-content">
       <div class="checkout-header-left-section">
-        <a href="amazon.html">
+        <a href="index.html">
           <img class="amazon-logo" src="images/amazon-logo.png">
           <img class="amazon-mobile-logo" src="images/amazon-mobile-logo.png">
         </a>
       </div>
 
       <div class="checkout-header-middle-section">
-        Checkout (<a class="return-to-home-link" href="amazon.html">${
+        Checkout (<a class="return-to-home-link" href="index.html">${
           quantityCheck() === 1 ? "1 item" : `${quantityCheck()} items`
         }</a>)
       </div>
